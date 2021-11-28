cdf17de5c381a3d42c61f213e7b71107d0bf56ee
Armandas <armandas0707@gmail.com>
2021-11-28 15:30:30 +0200

    WIP

--- internal/common_http/common_http.go
+++ internal/common_http/common_http.go
@@ -75,7 +75,8 @@ func (r *client) doRequest(ctx context.Context, method, address, token string, p
 func WriteJSONResponse(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers","Content-Type,access-control-allow-origin, access-control-allow-headers")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	w.WriteHeader(statusCode)
 
