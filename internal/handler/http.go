a9a94c551571639aef8207ce68fc5d57f7fa84cf
Armandas <armandas0707@gmail.com>
2021-11-28 17:05:21 +0200

    wip

--- internal/handler/http.go
+++ internal/handler/http.go
@@ -15,10 +15,10 @@ func HandleHTTP() {
 	// Basic CORS
 	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://3.123.229.48"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"*"}, // Use this to allow specific origin hosts
 		//AllowedOrigins:   []string{"*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"*"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
